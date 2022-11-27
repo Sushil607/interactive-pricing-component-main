8c459d9e732e8421c47f758bb96a383c641e3f0b
Sushil607 <bodadesushil@gmail.com>
2022-11-28 02:41:50 +0530

    write function for changing slider bg according to pageViews

--- script.js
+++ script.js
@@ -14,3 +14,13 @@ function changePageViews() {
   const pageViewsArray = ["10K", "50K", "100K", "500K", "1M"];
   pageViews.innerHTML = pageViewsArray[slider.value - 1];
 }
+
+/**
+ * ! Function for changing slider bg
+ * **/
+
+slider.addEventListener("input", () => {
+  var x = ((slider.value - slider.min) / (slider.max - slider.min)) * 100;
+  var color = `linear-gradient(90deg,hsl(174, 77%, 80%) ${x}%,hsl(224, 65%, 95%) ${x}%)`;
+  slider.style.cssText = `background:${color}`;
+});
