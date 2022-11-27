d7c218779a6c2ca3de498d4c444c55f987ea412a
Sushil607 <bodadesushil@gmail.com>
2022-11-28 03:34:43 +0530

    change changePageViews function to show discounted rates

--- script.js
+++ script.js
@@ -4,6 +4,9 @@
 
 const slider = document.getElementById("myRange");
 const pageViews = document.getElementById("pageViews");
+const perMonth = document.getElementById("perMonth");
+
+let billingMode = "monthly";
 
 // Initial value
 pageViews.innerHTML = "100K";
@@ -12,7 +15,19 @@ slider.addEventListener("change", changePageViews);
 
 function changePageViews() {
   const pageViewsArray = ["10K", "50K", "100K", "500K", "1M"];
+  const monthlyModeBilling = ["$8", "$12", "$16", "$24", "$36"];
+  const yearlyModeBilling = ["$6", "$9", "$12", "$18", "$27"];
   pageViews.innerHTML = pageViewsArray[slider.value - 1];
+  if (billingMode === "monthly") {
+    console.log("monthly");
+    perMonth.innerHTML = monthlyModeBilling[slider.value - 1];
+    return;
+  }
+  if (billingMode === "yearly") {
+    console.log("yearly");
+    perMonth.innerHTML = yearlyModeBilling[slider.value - 1];
+    return;
+  }
 }
 
 /**
@@ -23,4 +38,29 @@ slider.addEventListener("input", () => {
   var x = ((slider.value - slider.min) / (slider.max - slider.min)) * 100;
   var color = `linear-gradient(90deg,hsl(174, 77%, 80%) ${x}%,hsl(224, 65%, 95%) ${x}%)`;
   slider.style.cssText = `background:${color}`;
+});
+
+/**
+ * ! Function for toggling billing mode
+ * **/
+
+const toggleContainer = document.getElementsByClassName("toggle-container")[0];
+const toggleBtn = document.getElementsByClassName("toggle-btn")[0];
+
+toggleContainer.addEventListener("click", () => {
+  toggleBtn.classList.toggle("left");
+  toggleBtn.classList.toggle("right");
+
+  // Change billing mode based on class
+  if (toggleBtn.classList.contains("left")) {
+    billingMode = "monthly";
+    changePageViews();
+    return;
+  }
+
+  if (toggleBtn.classList.contains("right")) {
+    billingMode = "yearly";
+    changePageViews();
+    return;
+  }
 });
